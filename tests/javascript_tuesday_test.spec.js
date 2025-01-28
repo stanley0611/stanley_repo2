c7302426120499c67f03c1bef2ec24e65e68ab30
stanley <stanleysc611@gmail.com>
2025-01-28 19:55:19 +0530

    added comments

--- tests/javascript_tuesday_test.spec.js
+++ tests/javascript_tuesday_test.spec.js
@@ -27,7 +27,7 @@ test ("tuesday1", async() => {
 
 
 
-test.only ("Duplicate", async() =>{
+test ("Duplicate", async() =>{
 
 function duplicate(dup) {
     return dup.filter((value,index,arr) =>arr.indexOf(value) !== index);
@@ -38,4 +38,23 @@ let result1 = duplicate([12,85,85,96,36]);
 console.log('Result', result1)
 
 return result1
+})
+
+test ("Max value", async() => {
+
+function biggestvalue(bigv){
+    return Math.min(...bigv)
+}
+  let numbers = biggestvalue([52,96,85,63]);
+   console.log("Biggest Number :", numbers)
+})
+
+
+test.only ("Captial Letter", async() => {
+ function captialletter(caps) {
+    return caps.filter(letters => /[a-z]/.test(letters));
+ }
+    let result = captialletter(['A','B','C','a','k','g']);
+
+    console.log(result);
 })
