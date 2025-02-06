12e565a233dcf15694fa93cc43b39a9d8972a4f1
stanley <stanleysc611@gmail.com>
2025-02-06 12:50:57 +0530

    Added some code

--- tests/javascript_practice2.test.spec.js
+++ tests/javascript_practice2.test.spec.js
@@ -1,11 +1,49 @@
 const {test,expect} = require('@playwright/test')
 
 
-test ("JS Practice", async() => {
-    function addition(...addi){
-        //console.log("Entered")
-        return addi.reduce((sum, curr) => sum+curr, 0)
+// test ("JS Practice", async() => {
+//     function addition(...addi){
+       
+//         return addi.reduce((sum, curr) => sum+curr, 0)
+//     }
+//     let numbers = addition(1,2,3,4,5,6)
+//     console.log(numbers)
+// })
+
+
+
+test  ("Javascript Practice", async() => {
+    function javascriptPractice(JSP) {
+        return javascriptPractice = new Promise(function(resolve,response) {
+            let x = 25
+            if (x==25){
+                return resolve ("The value is okay")
+            }else {
+                return reject("The value is not okay")
+            }
+        })
     }
-    let numbers = addition(1,2,3,4,5,6)
-    console.log(numbers)
+    javascriptPractice()
+    .then(function(value){
+        console.log(value)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+})
+
+test ("First letter only", async() => {
+    function firstLetterOnly(flo) {
+        return flo.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+    }
+    let result = firstLetterOnly("your sample code goes here");
+    console.log(result)
+})
+
+test.only ("Last letter only", async() => {
+    function lastLetteronly(llo){
+        return llo.split(' ').map(word => word.slice(0,-1) + word.slice(-1).toUpperCase()).join(' ')
+    }
+    let result = lastLetteronly("great place to work is just a phrase")
+    console.log(result)
 })
